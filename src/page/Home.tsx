7edfeba757b4de4d8c6ebcdac23827b6a6c6539c
khakisage <jongjin201@gmail.com>
2024-09-10 22:54:42 +0900

    전체 상품 리스트 조회 페이지_1 - useQuery 적용

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import CustomButton from '../components/Button';
+import Typo from '../components/Typo';
+import {useProductList} from '../store/query/useGetProductList';
 
 function Home(): JSX.Element {
+  const pressBtn = () => {
+    console.log('Button Pressed');
+  };
+  const {isLoading, data: products} = useProductList();
+
   return (
     <View style={styles.container}>
       <Text>This is Home Screen</Text>
+      <CustomButton style={styles.buttonContainer} onPress={pressBtn}>
+        <Typo color="black">물건 등록</Typo>
+      </CustomButton>
     </View>
   );
 }
@@ -15,6 +26,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 60,
+    right: 50,
+    width: 120,
+    height: 40,
+    borderWidth: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#f2f222',
   },
 });
 
