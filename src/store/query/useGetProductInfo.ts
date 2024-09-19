d6c9dd858769cb873b2b569dd1c5b42300afdc55
khakisage <jongjin201@gmail.com>
2024-09-19 15:25:51 +0900

    상품 조회 기능 구현

--- src/store/query/useGetProductInfo.ts
+++ src/store/query/useGetProductInfo.ts
@@ -1,5 +1,24 @@
 import axios from 'axios';
+import { API_URL } from '../../../config';
+import { useQuery } from '@tanstack/react-query';
 
 const getProductInfo = async (id: number) => {
-  const response = await axios.get();
+  try {
+    const response = await axios.get(`${API_URL}/contents/listContents?contentsId=${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data.result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const useProductInfo = (id: number) => {
+  return useQuery({
+    queryKey: ['productInfo', id],
+    queryFn: () => getProductInfo(id),
+    enabled: true,
+  });
 };
