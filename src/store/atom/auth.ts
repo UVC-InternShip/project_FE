e478fca20527c3216061bbdedee881c125d73816
khakisage <jongjin201@gmail.com>
2024-10-20 14:08:41 +0900

    Refactor auth.ts to include userAtom for managing user information

--- src/store/atom/auth.ts
+++ src/store/atom/auth.ts
@@ -1,5 +1,8 @@
 // 로그인 상태 atom 관리
 
 import {atom} from 'jotai';
+import {IUserInfo} from '../../interface/interface';
 
 export const isLoggedInAtom = atom<boolean>(false);
+
+export const userAtom = atom<IUserInfo>();
