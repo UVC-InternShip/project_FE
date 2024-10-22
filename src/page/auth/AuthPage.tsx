1f443a891575a219185e02279228845fdff8dd1c
khakisage <jongjin201@gmail.com>
2024-10-22 14:41:03 +0900

    🎨 본인인증 진입 페이지 디자인

--- src/page/auth/AuthPage.tsx
+++ src/page/auth/AuthPage.tsx
@@ -18,18 +18,23 @@ function AuthPage({navigation}: AuthPageProps): JSX.Element {
   return (
     <View style={styles.outerContainer}>
       <View style={styles.container}>
-        <LocalImage localAsset={require('../../assets/logo.png')} width={100} height={100} />
-        <Typo color="black" fontSize={20} style={styles.text}>
-          필요없는 물건이 있나요?
-        </Typo>
-        <Typo color="black" fontSize={20} style={styles.text}>
-          있다면 우선 등록하세요!
-        </Typo>
+        <LocalImage localAsset={require('../../assets/mainImg.png')} width={200} height={200} />
+        <View style={styles.textContainer}>
+          <Typo color="black" fontSize={22} style={styles.text}>
+            필요없는 물건이 있나요?
+          </Typo>
+          <Typo color="black" fontSize={22} style={styles.text}>
+            우선 등록하세요!
+          </Typo>
+          <Typo color="black" fontSize={14} style={styles.subText}>
+            본인 인증을 진행 해주세요.
+          </Typo>
+        </View>
       </View>
       <View style={styles.bottomContainer}>
-        <CustomButton onPress={pressSignupBtn}>
-          <Typo color="black" fontSize={20}>
-            본인인증하기
+        <CustomButton onPress={pressSignupBtn} style={styles.button}>
+          <Typo color="black" fontSize={15} style={styles.font}>
+            본인 인증하기
           </Typo>
         </CustomButton>
       </View>
@@ -41,23 +46,48 @@ const styles = StyleSheet.create({
   outerContainer: {
     flexDirection: 'column',
     justifyContent: 'space-between',
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    flex: 1,
   },
   container: {
-    height: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 9,
+  },
+  textContainer: {
+    marginTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
   bottomContainer: {
-    height: '20%',
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  button: {},
-
+  button: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FDEE55',
+    borderRadius: 8,
+    paddingVertical: 15,
+  },
+  font: {
+    fontWeight: 600,
+    lineHeight: 20,
+  },
   text: {
-    marginTop: 10,
-    height: 40, // 텍스트 높이 설정
-    lineHeight: 40, // 텍스트 높이 설정
+    fontWeight: 500,
+    lineHeight: 30,
+    // marginTop: 10,
+    // height: 40, // 텍스트 높이 설정
+    // lineHeight: 40, // 텍스트 높이 설정
+  },
+  subText: {
+    fontWeight: 500,
+    color: '#9BA1AA',
+    marginTop: 5,
   },
 
   titleText: {},
